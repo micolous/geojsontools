a8922e55807db8b02a4b4856ae9c7953070c4577
Michael Farrell <micolous+git@gmail.com>
2015-10-18 20:57:34 +1100

    gtfs2geojson: work with zip files instead

--- gtfs2geojson.py
+++ gtfs2geojson.py
@@ -2,12 +2,12 @@
 """
 gtfs2geojson
 Converts GTFS data into GeoJSON format
-Copyright 2014 Michael Farrell <http://micolous.id.au>
+Copyright 2014-2015 Michael Farrell <http://micolous.id.au>
 
 License: 3-clause BSD, see COPYING
 """
 
-import geojson, argparse, csv
+import geojson, argparse, csv, zipfile
 from datetime import timedelta
 from decimal import Decimal
 
@@ -18,7 +18,7 @@ def gtfs_stops(stops_f, output_f):
 	:param stops_f file: Input 'stops.txt' file from the GTFS feed.
 	:param output_f file: Output GeoJSON file stream.
 	"""
-	stops_c = csv.reader(stops_f)
+	stops_c = csv.reader(gtfs.open('stops.txt', 'r'))
 
 	output_layer = geojson.FeatureCollection([])
 	# assume WGS84 CRS
@@ -59,7 +59,7 @@ def time_as_timedelta(time):
 		return None
 	return timedelta(hours=h, minutes=m, seconds=s)
 
-def gtfs_routes(routes_f, shapes_f, trips_f, stoptimes_f, output_f):
+def gtfs_routes(gtfs, output_f):
 	"""
 	For each route, convert it's 'shape' into a GeoJSON LineString, and make all
 	of it's attributes available.
@@ -73,7 +73,7 @@ def gtfs_routes(routes_f, shapes_f, trips_f, stoptimes_f, output_f):
 	"""
 	
 	# Load up the stop times so we can find which are the best routes.
-	stoptimes_c = csv.reader(stoptimes_f)
+	stoptimes_c = csv.reader(gtfs.open('stop_times.txt', 'r'))
 	header = stoptimes_c.next()
 	trip_id_col = header.index('trip_id')
 	arrtime_col = header.index('arrival_time')
@@ -105,14 +105,14 @@ def gtfs_routes(routes_f, shapes_f, trips_f, stoptimes_f, output_f):
 	# Load the shapes into a map that we can lookup.
 	# We should do all the geometry processing here so that we only have to do
 	# this once-off.
-	shapes_c = csv.reader(shapes_f)
+	shapes_c = csv.reader(gtfs.open('shapes.txt', 'r'))
 
 	header = shapes_c.next()
 	shape_id_col = header.index('shape_id')
 	shape_lat_col = header.index('shape_pt_lat')
 	shape_lng_col = header.index('shape_pt_lon')
 	shape_seq_col = header.index('shape_pt_sequence')
-	shape_dist_col = header.index('shape_dist_traveled')
+	shape_dist_col = header.index('shape_dist_traveled') if 'shape_dist_traveled' in header else None
 
 	shapes = {}
 	shape_lengths = {}
@@ -124,9 +124,10 @@ def gtfs_routes(routes_f, shapes_f, trips_f, stoptimes_f, output_f):
 
 		# Calculate length according to GTFS
 		# This could also be calculated by the geometry, but we trust GTFS, right...
-		length = Decimal(row[shape_dist_col])
-		if row[shape_id_col] not in shape_lengths or shape_lengths[row[shape_id_col]] < length:
-			shape_lengths[row[shape_id_col]] = length
+		if shape_dist_col is not None:
+			length = Decimal(row[shape_dist_col])
+			if row[shape_id_col] not in shape_lengths or shape_lengths[row[shape_id_col]] < length:
+				shape_lengths[row[shape_id_col]] = length
 
 	# translate the shapes into a LineString for use by the GeoJSON module
 	for shape_id in shapes.iterkeys():
@@ -143,7 +144,7 @@ def gtfs_routes(routes_f, shapes_f, trips_f, stoptimes_f, output_f):
 	trips_ref = {}
 	route_time = {}
 
-	trips_c = csv.reader(trips_f)
+	trips_c = csv.reader(gtfs.open('trips.txt', 'r'))
 	header = trips_c.next()
 	route_id_col = header.index('route_id')
 	shape_id_col = header.index('shape_id')
@@ -183,7 +184,7 @@ def gtfs_routes(routes_f, shapes_f, trips_f, stoptimes_f, output_f):
 	output_layer.crs = geojson.crs.Named('urn:ogc:def:crs:OGC:1.3:CRS84')
 
 	# now we have all the shapes available, translate the routes
-	routes_c = csv.reader(routes_f)
+	routes_c = csv.reader(gtfs.open('routes.txt', 'r'))
 	header = routes_c.next()
 	route_id_col = header.index('route_id')
 
@@ -194,9 +195,14 @@ def gtfs_routes(routes_f, shapes_f, trips_f, stoptimes_f, output_f):
 			if row[i] != '':
 				props[h] = row[i]
 
-		props['shape_id'] = trips[row[route_id_col]]
+		try:
+			props['shape_id'] = trips[row[route_id_col]]
+		except KeyError:
+			print 'invalid route_id with missing shape = %r' % (row[route_id_col],)
+			continue
 		props['shape_refs'] = trips_ref[row[route_id_col]][props['shape_id']]
-		props['shape_length'] = shape_lengths[props['shape_id']]
+		if shape_dist_col is not None:
+			props['shape_length'] = shape_lengths[props['shape_id']]
 		props['duration_sec'] = (route_time[row[route_id_col]][3] - route_time[row[route_id_col]][2]).total_seconds()
 
 		output_layer.features.append(geojson.Feature(
@@ -220,46 +226,31 @@ def main():
 		help='Output GeoJSON file'
 	)
 
-	group = parser.add_argument_group(title='Route conversion')
+	parser.add_argument('input_gtfs',
+		type=argparse.FileType('rb'),
+		help='Path to GTFS ZIP file to extract data from.')
+	
+	group = parser.add_mutually_exclusive_group()
+	
 	group.add_argument('-r', '--routes',
-		type=argparse.FileType('rb'),
-		help='Path to agency\'s `routes.txt` file.'
-	)
-
-	group.add_argument('-s', '--shapes',
-		type=argparse.FileType('rb'),
-		help='Path to agency\'s `shapes.txt` file.'
-	)
-
-	group.add_argument('-t', '--trips',
-		type=argparse.FileType('rb'),
-		help='Path to agency\'s `trips.txt` file.'
-	)
-
-	group.add_argument('-i', '--stop-times',
-		type=argparse.FileType('rb'),
-		help='Path to agency\'s `stop_times.txt` file.'
-	)
-
-	group = parser.add_argument_group(title='Stop conversion')
-	group.add_argument('-p', '--stops',
-		type=argparse.FileType('rb'),
-		help='Path to agency\'s `stops.txt` file.'
-	)
+		action='store_true',
+		help='Route conversion mode')
+	
+	group.add_argument('-s', '--stops',
+		action='store_true',
+		help='Stop conversion mode')
 
 	options = parser.parse_args()
 
-	if options.routes and not options.stops:
-		assert options.shapes
-		assert options.trips
-		assert options.stop_times
+	assert options.routes or options.stops
 
-		gtfs_routes(options.routes, options.shapes, options.trips, options.stop_times, options.output)
-	elif options.stops and not options.routes:
-		gtfs_stops(options.stops, options.output)
-	else:
-		print 'invalid options combination'
-
+	# Open ZIP
+	gtfs = zipfile.ZipFile(options.input_gtfs, 'r')
+	
+	if options.routes:
+		gtfs_routes(gtfs, options.output)
+	elif options.stops:
+		gtfs_stops(gtfs, options.output)
 
 if __name__ == '__main__':
 	main()
